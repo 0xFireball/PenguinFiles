d778d45d3f862be05dd14b548c465739884f0764
0xFireball <0xFireball@outlook.com>
2016-11-06 23:08:57 -0800

    update build output path

--- PenguinFiles/src/penguinfiles/config/index.js
+++ PenguinFiles/src/penguinfiles/config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../../PenguinFiles.Server/wwwroot/app/index.html'),
+    assetsRoot: path.resolve(__dirname, '../../PenguinFiles.Server/wwwroot/app'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
