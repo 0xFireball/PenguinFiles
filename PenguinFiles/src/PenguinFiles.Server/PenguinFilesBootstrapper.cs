6a5610ed849d833536206c89801bb4e6a6e45950
0xFireball <0xFireball@outlook.com>
2016-11-06 22:58:16 -0800

    create project

---
+++ PenguinFiles/src/PenguinFiles.Server/PenguinFilesBootstrapper.cs
@@ -0,0 +1,46 @@
+﻿using Nancy;
+using Nancy.Authentication.Forms;
+using Nancy.Bootstrapper;
+using Nancy.Conventions;
+using Nancy.Session;
+using Nancy.TinyIoc;
+
+namespace PenguinFiles
+{
+    public class PenguinFilesBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+            nancyConventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("assets", "wwwroot/assets/")
+            );
+            nancyConventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("static", "wwwroot/static/")
+            );
+            nancyConventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("app", "wwwroot/app/")
+            );
+        }
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            CookieBasedSessions.Enable(pipelines);
+        }
+
+        protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+        {
+            base.RequestStartup(container, pipelines, context);
+
+            var formsAuthConfiguration =
+            new FormsAuthenticationConfiguration
+            {
+                RedirectUrl = "/app/login",
+                UserMapper = container.Resolve<IUserMapper>()
+            };
+
+            FormsAuthentication.Enable(pipelines, formsAuthConfiguration);
+        }
+    }
+}
