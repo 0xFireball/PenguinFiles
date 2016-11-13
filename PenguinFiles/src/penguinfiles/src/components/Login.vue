c06595f97923fdb7c7d0b64558384cade197a4a1
0xFireball <0xFireball@outlook.com>
2016-11-13 13:21:30 -0800

    use vue material

--- PenguinFiles/src/penguinfiles/src/components/Login.vue
+++ PenguinFiles/src/penguinfiles/src/components/Login.vue
@@ -1,24 +1,27 @@
 <template>
   <div class="login">
     <h3>Log In / Sign Up</h3>
-    <ui-tabs type="text">
-      <ui-tab header="Books">
-        My books
-        <a href="https://google.com" target="_blank" rel="noopener">Hey</a>
-      </ui-tab>
+    <md-tabs>
+      <md-tab id="movies" md-label="Movies">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas
+          voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
+      </md-tab>
 
-      <ui-tab header="Authors">
-        Authors
-      </ui-tab>
+      <md-tab id="music" md-label="Music">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas
+          voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas
+          voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
+      </md-tab>
 
-      <ui-tab header="Collections">
-        My collections
-      </ui-tab>
+      <md-tab id="books" md-label="Books">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
+      </md-tab>
 
-      <ui-tab header="Favourites">
-        My favourites
-      </ui-tab>
-    </ui-tabs>
+      <md-tab id="pictures" md-label="Pictures">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
+      </md-tab>
+    </md-tabs>
     <form v-on:submit.prevent="tryLogin">
       <div class="row">
         <div class="six columns">
--- PenguinFiles/src/penguinfiles/src/main.js
+++ PenguinFiles/src/penguinfiles/src/main.js
@@ -2,20 +2,19 @@ import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
 
-// UI
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.css'
-
-
-// Register plugins
-Vue.use(VueRouter)
-Vue.use(VueMaterial)
-
 // Styles
 import './assets/styles/global.css'
 import 'skeleton-css/css/normalize.css'
 import 'skeleton-css/css/skeleton.css'
 import 'keen-ui/dist/keen-ui.css'
+
+// UI
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
+
+// Register plugins
+Vue.use(VueRouter)
+Vue.use(VueMaterial)
 
 /* eslint-disable no-new */
 new Vue({
