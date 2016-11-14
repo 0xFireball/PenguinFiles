0fb577cecf6066162e4f5e01992f8411a5382d8f
0xFireball <0xFireball@outlook.com>
2016-11-13 18:19:09 -0800

    minor ui tweaks

--- PenguinFiles/src/penguinfiles/src/components/Login.vue
+++ PenguinFiles/src/penguinfiles/src/components/Login.vue
@@ -36,11 +36,15 @@
         </form>
       </md-tab>
 
-      <md-tab id="t-about" md-label="About">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas
-          voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas
-          voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
+      <md-tab id="t-about" md-label="About" v-md-ink-ripple>
+        <h5>About PenguinFiles</h5>
+        <p>
+          PenguinFiles will be announced soon!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem,
+          expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.
+        </p>
       </md-tab>
     </md-tabs>
   </div>
