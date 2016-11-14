1c078caaa8731eb80966db9dee51d71678bfc27d
0xFireball <0xFireball@outlook.com>
2016-11-13 17:51:57 -0800

    add signup form

--- PenguinFiles/src/penguinfiles/src/components/Login.vue
+++ PenguinFiles/src/penguinfiles/src/components/Login.vue
@@ -13,9 +13,28 @@
             <md-input type="password"></md-input>
           </md-input-container>
         </form>
+        <md-button class="md-raised md-primary" v-on:click="tryLogin">Log In</md-button>
       </md-tab>
 
       <md-tab id="t-signup" md-label="Sign Up">
+        <form v-on:submit.prevent="trySignup">
+          <md-input-container>
+            <label>Username</label>
+            <md-input></md-input>
+          </md-input-container>
+          <md-input-container md-has-password>
+            <label>Password</label>
+            <md-input type="password"></md-input>
+          </md-input-container>
+          <md-input-container>
+            <label>Confirm Password</label>
+            <md-input type="password"></md-input>
+          </md-input-container>
+        </form>
+        <md-button class="md-raised md-primary" v-on:click="trySignup">Sign Up</md-button>
+      </md-tab>
+
+      <md-tab id="t-about" md-label="About">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas
           voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas
@@ -35,7 +54,11 @@ export default {
   methods: {
     tryLogin: function () {
       // nothing
-      window.alert('Can\'t submit yet.')
+      window.alert('Can\'t log in yet.')
+    },
+    trySignup: function () {
+      // nothing
+      window.alert('Can\'t sign up yet.')
     }
   }
 }
