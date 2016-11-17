83ae5bf1b988423743073f651efd860dd4bfdb20
0xFireball <0xFireball@outlook.com>
2016-11-16 20:54:41 -0800

    add sidebar navigation

--- PenguinFiles/src/penguinfiles/src/components/Toolbar.vue
+++ PenguinFiles/src/penguinfiles/src/components/Toolbar.vue
@@ -14,13 +14,55 @@
     <md-sidenav class="md-left" ref="leftSidenav">
       <md-toolbar class="md-large">
         <div class="md-toolbar-container">
-          <h3 class="md-title">{{ appName }}</h3>
+          <h2 class="md-title">{{ appName }}</h2>
         </div>
       </md-toolbar>
+      <!--
       <div class="container toolbar-content">
         <h3>GitHub</h3>
         <p>Star {{ appName }} on GitHub!</p>
         <md-button class="md-raised md-accent" @click="visitGitHub">Star</md-button>
+      </div>
+      -->
+      <div class="toolbar-content">
+        <!--
+        <div class="row">
+          <div class="twelve columns">
+            
+          </div>
+        </div>
+        -->
+        <div class="sidebar-links">
+          <md-list class="md-dense">
+            <md-list-item>
+              <router-link exact to="/">Home</router-link>
+            </md-list-item>
+
+            <md-list-item>
+              <router-link exact to="/files">Files</router-link>
+            </md-list-item>
+
+            <md-list-item>
+              <router-link exact to="/about">About</router-link>
+            </md-list-item>
+
+            <md-list-item>
+              <span>Support</span>
+
+              <md-list-expand>
+                <md-list>
+                  <md-list-item class="md-inset" href="https://github.com/0xFireball/PenguinFiles">
+                    GitHub
+                  </md-list-item>
+
+                  <md-list-item class="md-inset" href="https://github.com/0xFireball">
+                    Donate
+                  </md-list-item>
+                </md-list>
+              </md-list-expand>
+            </md-list-item>
+          </md-list>
+        </div>
       </div>
     </md-sidenav>
   </div>
@@ -47,7 +89,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .toolbar-content {
-    margin-top: 15%;
-  }
+  .toolbar-content {}
 </style>
